d5dafdc517fa631e66e7134027ba2a18fe6c39ae
queguangwei <queguangwei@ddy98.com>
2019-03-09 00:41:17 +0800

    'commit'

--- src/views/Deal.jsx
+++ src/views/Deal.jsx
@@ -6,122 +6,10 @@ import { bindActionCreators } from 'redux'
 import ApiCaller from '../utils/ApiCaller'
 import Api from '../constants/Api'
 import { NavBar, Icon, Modal, Picker, InputItem, List, Stepper, WhiteSpace } from 'antd-mobile'
-import { createForm } from 'rc-form'
 
 const alert = Modal.alert;
 let finialValues = {}
 
-class Form extends  React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			val: 1,
-			keys: [1]
-		};
-	}
-	handleSubmit(e) {
-		e.preventDefault();
-		this.props.form.validateFields({ force: true }, (error, value) => {
-			console.log(value)
-			if (!error) {
-				console.log(this.props.form.getFieldsValue());
-			} else {
-				console.log(error);
-				alert('Validation failed');
-			}
-		});
-
-	}
-	onChange(val) {
-		console.log(val);
-		let keys = []
-		for(let i = 1;i <= val;i ++) {
-			keys.push(val)
-		}
-		this.setState({ val, keys });
-	}
-	render() {
-		const { getFieldProps } = this.props.form;
-		const lessons = [
-			{label: '男', value: 'male'},
-			{label: '女', value: 'female'},
-			{label: 'A类', value: 'classA'},
-			{label: 'B类', value: 'classB'},
-			{label: 'C类', value: 'classC'}
-		]
-		const formItems = this.state.keys.map((k, index) => {
-			return (
-				<div >
-					<InputItem
-						{...getFieldProps('name')}
-						clear
-						placeholder="请输入信息"
-						style={{textAlign:'right'}}
-					>姓名</InputItem>
-					<InputItem
-						{...getFieldProps('mobile')}
-						type="phone"
-						clear
-						placeholder="请输入信息"
-						style={{textAlign:'right'}}
-					>电话</InputItem>
-					<InputItem
-						{...getFieldProps('post')}
-						clear
-						placeholder="请输入信息"
-						style={{textAlign:'right'}}
-					>职位</InputItem>
-				</div>
-			)
-		});
-		return (
-			<form onSubmit={this.handleSubmit.bind(this)}>
-				<List>
-					<Picker data={lessons} cols={1} {...getFieldProps('lesson')}>
-						<List.Item arrow="horizontal">课程名称</List.Item>
-					</Picker>
-					<InputItem
-						{...getFieldProps('companynNme')}
-						type="money"
-						clear
-						placeholder="请输入信息"
-					>课程单价</InputItem>
-					<List.Item
-						wrap
-						extra={
-							<Stepper
-								style={{ width: '100%', minWidth: '100px' }}
-								showNumber
-								max={10}
-								min={1}
-								value={this.state.val}
-								onChange={this.onChange.bind(this)}
-							/>}
-					>
-						人数
-					</List.Item>
-				</List>
-				<WhiteSpace size="xs"/>
-				<List>
-					{formItems}
-
-
-				</List>
-				<WhiteSpace />
-				<List>
-					<InputItem
-						{...getFieldProps('companynNme')}
-						type="money"
-						clear
-						placeholder="请输入信息"
-					>费用</InputItem>
-				</List>
-			</form>
-		)
-	}
-
-}
-const FormWrapper = createForm()(Form);
 
 class Deal extends React.Component {
 	constructor(props) {
@@ -130,6 +18,8 @@ class Deal extends React.Component {
             userId: '',
             lessonList: [],
 			data: [],
+			val: 1,
+			keys: [1]
 		}
 	}
 
@@ -186,13 +76,57 @@ class Deal extends React.Component {
         })
     }
 
+	onChange(val) {
+		console.log(val);
+		let keys = []
+		for(let i = 1;i <= val;i ++) {
+			keys.push(val)
+		}
+		this.setState({ val, keys });
+	}
+
 	componentDidMount() {
         this.setState({userId: this.props.location.query.id})
         this.getLessonList()
 	}
 
 	render() {
-
+		const state = this.state;
+		const lessons = [
+			{label: '男', value: 'male'},
+			{label: '女', value: 'female'},
+			{label: 'A类', value: 'classA'},
+			{label: 'B类', value: 'classB'},
+			{label: 'C类', value: 'classC'}
+		]
+		const formItems = this.state.keys.map((k, index) => {
+			return (
+				<div style={{background:'#F0F0F0'}}>
+					<List>
+						<InputItem
+							value={state.name}
+							clear
+							placeholder="请输入信息"
+							style={{textAlign:'right'}}
+						>姓名</InputItem>
+						<InputItem
+							value={state.code}
+							type="phone"
+							clear
+							placeholder="请输入信息"
+							style={{textAlign:'right'}}
+						>电话</InputItem>
+						<InputItem
+							value={state.job}
+							clear
+							placeholder="请输入信息"
+							style={{textAlign:'right'}}
+						>职位</InputItem>
+					</List>
+					<WhiteSpace size="xs"/>
+				</div>
+			)
+		});
 		return (
 			<div>
 				<NavBar
@@ -203,9 +137,49 @@ class Deal extends React.Component {
 				>
 					新增课程
 				</NavBar>
-				<div className="">
-					<FormWrapper onSubmit={this.handleSubmit.bind(this)}/>
-				</div>
+				<form onSubmit={this.handleSubmit.bind(this)}>
+					<List>
+						<Picker
+							data={lessons}
+							cols={1}
+							value={state.lesson}
+						>
+							<List.Item arrow="horizontal">课程名称</List.Item>
+						</Picker>
+						<InputItem
+							value={state.companynNme}
+							type="money"
+							clear
+							placeholder="请输入信息"
+						>课程单价</InputItem>
+						<List.Item
+							wrap
+							extra={
+								<Stepper
+									style={{ width: '100%', minWidth: '100px' }}
+									showNumber
+									max={10}
+									min={1}
+									value={state.val}
+									onChange={this.onChange.bind(this)}
+								/>}
+						>
+							人数
+						</List.Item>
+						<InputItem
+							value={state.fee}
+							type="money"
+							clear
+							placeholder="请输入信息"
+						>费用</InputItem>
+					</List>
+					<WhiteSpace />
+					<List>
+						{formItems}
+
+					</List>
+
+				</form>
 			</div>
 		)
 	}
